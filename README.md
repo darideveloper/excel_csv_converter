ff5559e0eddef7bfe0c089b48415b29119916b9c
dari <hernandezdarifrancisco@gmail.com>
2020-12-22 22:46:49 -0600

    Xlsx to Csv and update README
    Convert last code to OOP
    Crate function to generate csv files from xlsx file
    Update README.md to structure: title, install, how to use

--- README.md
+++ README.md
@@ -1,23 +1,15 @@
-# excel to csv
-## Description
+# Excel csv converter
 This is a project without GUI. 
-**Convert** each page of a **Excel (xlsx)** file, or path with xlsx files, **to cvs files**. 
-## How to use
-### Install modules
-```bash
-$ pip3 install openpyxl
+**Convert** each page of a **Excel (xlsx)** file, or path with xlsx files, **to cvs files**.
+
+# Install
+``` bash
+$
 ```
-### Run the program
-```bash
-# Convert one file and save the csv files in specific folder
-$ python3 main.py "usr/myPath/file.xlsx" "usr/destinyPath"** 
 
-# Save the csv files in parent folder
-$ python3 main.py "usr/myPath/file.xlsx"** 
 
-# Convert all xlsx files froma path and save the csv files in specific folder
-$ python3 main.py "usr/myPath" "usr/destinyPath"** 
+# How to use
 
-# Convert all xlsx files from a path and save the csv in parent folder
-$ python3 main.py "usr/myPath"** 
+bash ```
+$
 ```
--- excel_csv_converter/converter.py
+++ excel_csv_converter/converter.py
@@ -1,67 +1,85 @@
 #! python3
 # Conver each sheet for xlsx file to csv files
 
-import os, openpyxl, csv, sys
+import os, openpyxl, csv, sys, errno
 
-# Red the terminal
-helpMenssage = """Write the path of the .xlsx file, and opcional the destination 
-folder to the .csv files (example: main.py "user/myfolder/file.xlsx" "user/folderCSV"). 
-If you only type the xlsx file path, the csv files will make in the parent folder (example: main.py "user/myfolder/file.xlsx")
-If you have more that one file, type the parent folder of all xlsx files (example: main.py "user/myfolder") 
-"""
+class Convert (): 
+    """
+    Convert each page from xlsx file, to csv files,
+    or insert csv information in xlsx file
+    """
 
-if len(sys.argv) == 1: 
-    print ('The program need more arguments. ' + helpMenssage)
-    sys.exit()
-elif len(sys.argv) == 2:  
-    pathXlsx = sys.argv[1]
-    pathDestiny = os.path.dirname(pathXlsx)
-elif len(sys.argv) == 3:  
-    pathXlsx = sys.argv[1]
-    pathDestiny = sys.argv[2]
-else: 
-    print ('To much argument. ' + helpMenssage)
-    sys.exit()
+    def __init__ (self, file_csv = "", file_xlsx = ""):
+        """ 
+        Constructor of class. Get the from and to path files
+        """
 
-# Verify the paths
-if not os.path.exists (pathXlsx) or not os.path.exists (pathDestiny): 
-    print ('Check your paths')
-    sys.exit()
+        self.file_csv = file_csv
+        self.file_xlsx = file_xlsx
 
-def convertXlsxToCsv (excelFile): 
-    """ Convert each sheet of a xlsx file to a csv file"""
-    wb = openpyxl.load_workbook(excelFile)
+        if self.file_csv != "": 
+            self.__verify_path (self.file_csv) 
+            self.__verify_extension (self.file_csv, ".csv")
+        
+        if self.file_xlsx != "": 
+            self.__verify_path (self.file_xlsx)
+            self.__verify_extension (self.file_xlsx, ".xlsx")
 
-    for sheetName in wb.sheetnames: 
+    def __verify_path (self, path):
+        """
+        Verify is the from file and the to file path exist in the pc
+        """ 
+
+        if not os.path.exists (path): 
+            raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), path)
+        
+    def __verify_extension (self, file, extension):
+        """
+        Verify is the from file and the to file path exist in the pc
+        """ 
+
+        if not file.endswith (extension): 
+            raise ValueError(errno.ENOENT, os.strerror(errno.ENOENT), file)
+
+    def xlsx_to_csv (self, destionarion_folder): 
+        """
+        Convert each page from csv file to csv files
+        """
+
+
+        self.__verify_path (destionarion_folder)
+    
+        wb = openpyxl.load_workbook(self.file_xlsx)
+
         # Loop through every sheet in the workbook
-        sheet = wb[sheetName]
+        for sheetName in wb.sheetnames: 
+            sheet = wb[sheetName]
 
-        #Crate the csv filename drom the xlsx file name and the sheet title.
-        csvFile = excelFile[:-5] + '_' + sheetName + '.csv'
-        csvPath = os.path.join(pathDestiny, csvFile)
-        outputFile = open(csvPath, 'w', newline='')
+            #Crate the csv filename drom the xlsx file name and the sheet title.
+            csvFile = os.path.basename(self.file_xlsx)[:-5] + '_' + sheetName + '.csv'
+            csvPath = os.path.join(destionarion_folder, csvFile)
+            outputFile = open(csvPath, 'w', newline='')
 
-        #crate the csv.writer object for this csv file
-        outputWriter = csv.writer(outputFile)
+            #crate the csv.writer object for this csv file
+            outputWriter = csv.writer(outputFile)
 
-        # Loop though every row un the sheet
-        for rowNum in range (1, sheet.max_row + 1): 
-            rowData = [] #Appen each cell to this list
-            #Loop trough each cell in the row
-            for colNum in range (1, sheet.max_column + 1): 
-                #Appen each cell's data to row data
-                rowData.append(sheet.cell(rowNum, colNum).value)            
-            #Write  the rowData list to the csv file.
-            outputWriter.writerow(rowData)
-        outputFile.close()
-        print ("File %s generated." % (csvPath))
+            # Loop though every row un the sheet
+            for rowNum in range (1, sheet.max_row + 1): 
 
-if pathXlsx.endswith('.xlsx'): 
-    convertXlsxToCsv (pathXlsx)
-else:
-    for currentExcelFile in os.listdir(pathXlsx): 
-        # Only convert xlsx files
-        if str(currentExcelFile).endswith('.xlsx'): 
-            convertXlsxToCsv (currentExcelFile)
+                rowData = [] #Appen each cell to this list
+
+                #Loop trough each cell in the row
+                for colNum in range (1, sheet.max_column + 1): 
+
+                    #Appen each cell's data to row data
+                    rowData.append(sheet.cell(rowNum, colNum).value)   
+
+                #Write  the rowData list to the csv file.
+                outputWriter.writerow(rowData)
+
+            outputFile.close()
+            print ("File %s generated." % (csvPath))
+
             
+
 
