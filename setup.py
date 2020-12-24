c225e55e2a4d9772ba165a881ad773393a52474a
dari <hernandezdarifrancisco@gmail.com>
2020-12-23 23:55:35 -0600

    Hotfix version details

--- setup.py
+++ setup.py
@@ -9,20 +9,20 @@ with open(os.path.join(this_directory, 'README.md')) as f:
 
 setup (
 	name = "excel_csv_converter",
-	version = "1.0.0", 
-	description = "Package that allows, with regular expressions, to extract from texts, phone numbers and emails",
+	version = "1.0.1", 
+	description = "Convert each page from xlsx file to csv files, convert file csv to xlsx document, insert csv data in existing file",
 	long_description = long_description,
 	long_description_content_type = "text/markdown",
 	author = "Dari Developer",
 	author_email = "hernandezdarifrancisco@gmail.com",
 	license = "MIT",
-	keywords = "extract, re, phones, emails",
+	keywords = "xlsx, csv, excel, extract, convert",
 	project_urls = {
-		"Documentation": "https://github.com/DariHernandez/phone_and_email_extractor/blob/master/README.md",
+		"Documentation": "https://github.com/DariHernandez/excel_csv_converter/blob/master/README.md",
 		"Funding": "https://www.paypal.com/paypalme/FranciscoDari",
-		"Source": "https://github.com/DariHernandez/phone_and_email_extractor"
+		"Source": "https://github.com/DariHernandez/excel_csv_converter/"
 		},
 	packages = find_packages(include=["excel_csv_converter", "excel_csv_converter.*"]),
-	install_requires = ["pyperclip"],
+	install_requires = ["openpyxl"],
 	python_requires = ">=3.7"
 )
